cb4c7a3d7812e269ce8d5210a9507bd41b0676a8
Diego Roa <droa@xolit.com>
2023-01-02 11:05:23 -0500

    Add project files.

---
+++ DatabaseAccess.EntityFramework/BankDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccess.EntityFramework
+{
+    public partial class BankDBContext : DbContext
+    {
+        public BankDBContext(DbContextOptions<BankDBContext> options)
+        : base(options) 
+        { }
+    }
+}
